f5a3e3fd07b633fffd1e9483b8cb6aa6998b0ff7
Suhirtha Raj <suhirtha@fb.com>
2018-07-16 10:00:12 -0700

    Created buttons in SelectionActivity that lead to two other screens

--- app/src/main/java/com/example/suhirtha/randomadventure/SelectionActivity.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/SelectionActivity.java
@@ -1,13 +1,37 @@
 package com.example.suhirtha.randomadventure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectionActivity extends AppCompatActivity {
+
+    Button mSearch;
+    Button mDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        mSearch = findViewById(R.id.btnSearch);
+
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(this, SelectionActivity.class, RandomizeActivity.class);
+            }
+        });
+
+        mDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(this, SelectionActivity.class, ResultActivity.class);
+            }
+        });
+
+
     }
 }
