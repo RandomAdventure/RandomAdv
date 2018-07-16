95106b017d29c56ef77668c14b60e088bc7e20c3
Annamary Cartagena <anitac@fb.com>
2018-07-16 15:55:06 -0700

    Creating anim

--- app/src/main/java/com/example/suhirtha/randomadventure/Activities/RandomizeActivity.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/Activities/RandomizeActivity.java
@@ -2,14 +2,31 @@ package com.example.suhirtha.randomadventure.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.suhirtha.randomadventure.R;
 
 public class RandomizeActivity extends AppCompatActivity {
+    Button rButton;
+    ImageView spinWheel;
+    //Animation clockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.clockwise);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_randomize);
+        rButton = findViewById(R.id.raButton);
+        spinWheel = findViewById(R.id.raImageView);
+
+    }
+
+    public void onClickRandom(View view) {
+        //clockwise.setTarget(spinWheel);
+        //clockwise.setDuration(3000);
+        //spinWheel.startAnimation(clockwise);
+
     }
 }
