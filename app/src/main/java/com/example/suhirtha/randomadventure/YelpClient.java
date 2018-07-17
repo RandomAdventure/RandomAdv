8875976e745e580287a615cd64626f46409ce85b
Tatum Ogata <togata@fb.com>
2018-07-17 13:36:37 -0700

    added okhttp dependency

--- app/src/main/java/com/example/suhirtha/randomadventure/YelpClient.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/YelpClient.java
@@ -2,6 +2,16 @@ package com.example.suhirtha.randomadventure;
 
 import android.content.Context;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
 
 /**
  * Created by togata on 7/16/18.
@@ -14,8 +24,35 @@ public class YelpClient{
     public static final String REST_CLIENT_ID = "tTmiwin5LV1jvFNL51ng4A";
     public static final String REST_CALLBACK_URL_TEMPLATE = "intent://%s#Intent;action=android.intent.action.VIEW;scheme=%s;package=%s;S.browser_fallback_url=%s;end";
 
+    private final OkHttpClient client = new OkHttpClient();
+
+    public void run() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://publicobject.com/helloworld.txt")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    System.out.println(responseBody.string());
+                }
+            }
+        });
+    }
+
     public YelpClient(Context c) {
-        //super(c, REST_API_INSTANCE, REST_URL, REST_CLIENT_ID, REST_API_KEY, REST_CALLBACK_URL_TEMPLATE);
+
     }
 
 }
