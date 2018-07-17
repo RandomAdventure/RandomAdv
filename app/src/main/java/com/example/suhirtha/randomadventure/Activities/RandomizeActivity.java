99f8cf540be0bb63ddf82811cd766bad37a10dd1
Annamary Cartagena <anitac@fb.com>
2018-07-17 13:39:25 -0700

    Funtioning animation to rotating circle placeholder

--- app/src/main/java/com/example/suhirtha/randomadventure/Activities/RandomizeActivity.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/Activities/RandomizeActivity.java
@@ -1,8 +1,11 @@
 package com.example.suhirtha.randomadventure.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -11,7 +14,8 @@ import com.example.suhirtha.randomadventure.R;
 public class RandomizeActivity extends AppCompatActivity {
     Button rButton;
     ImageView spinWheel;
-    //Animation clockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.clockwise);
+    Context context;
+    Animation clockwise;
 
 
     @Override
@@ -20,13 +24,15 @@ public class RandomizeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_randomize);
         rButton = findViewById(R.id.raButton);
         spinWheel = findViewById(R.id.raImageView);
+        context = getApplicationContext();
+        clockwise = AnimationUtils.loadAnimation(context,R.anim.clockwise);
 
     }
 
     public void onClickRandom(View view) {
         //clockwise.setTarget(spinWheel);
         //clockwise.setDuration(3000);
-        //spinWheel.startAnimation(clockwise);
+        spinWheel.startAnimation(clockwise);
 
     }
 }
