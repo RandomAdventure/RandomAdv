2494982377989cc2d321c7b003ef4e6980f90dcb
Suhirtha Raj <suhirtha@fb.com>
2018-07-17 17:13:30 -0700

    Added seekBar for radius

--- app/src/main/java/com/example/suhirtha/randomadventure/SelectionActivity.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/SelectionActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.suhirtha.randomadventure.Activities.RandomizeActivity;
 
@@ -12,6 +15,8 @@ public class SelectionActivity extends AppCompatActivity {
 
     Button mSearch;
     Button mDone;
+    SeekBar mRadius;
+    TextView mRadiusDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,8 @@ public class SelectionActivity extends AppCompatActivity {
 
         mSearch = findViewById(R.id.btnSearch);
         mDone = findViewById(R.id.btnDone);
+        mRadius = findViewById(R.id.sbRadius2);
+        mRadiusDisplay = findViewById(R.id.tvDisplayRadius);
 
         mSearch.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +44,32 @@ public class SelectionActivity extends AppCompatActivity {
             }
         });
 
+        // Initialize the textview with '0'.
+        mRadiusDisplay.setText("Covered: " + mRadius.getProgress() + "/" + mRadius.getMax());
 
+        mRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mRadiusDisplay.setText("Covered: " + progress + "/" + seekBar.getMax());
+                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+
 }
+
