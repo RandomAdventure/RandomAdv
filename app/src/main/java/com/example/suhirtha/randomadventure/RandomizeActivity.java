2ff3b3ceb128082a8396e0795711b666218d892a
Annamary Cartagena <anitac@fb.com>
2018-07-19 16:11:23 -0700

    Tests changed from type string to type ArrayList<Restaurant>

--- app/src/main/java/com/example/suhirtha/randomadventure/RandomizeActivity.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/RandomizeActivity.java
@@ -14,24 +14,36 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.suhirtha.randomadventure.models.Restaurant;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomizeActivity extends AppCompatActivity {
     ImageView spinWheel;
     TextView[] res = new TextView[5];
     Context context;
+    YelpClient client;
+    ArrayList<Restaurant> testRestaurants;
+    ArrayList<Restaurant> testChosen;
     Animation clockwise, clockwise1, clockwise2 ,clockwise3, clockwise4;
     Random random = new Random();
     int generator;
     int REQUEST_CODE_SELECTION = 10;
     String[] restaurants= {"Pizzeria", "Italian", "Mexican", "Chuck E Cheese", "Fancy", "Barbeque", "French", "Brazilian", "Cuban", "Vegetarian"}; //hardcode
     String[] chosen = new String[5];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_randomize);
         spinWheel = findViewById(R.id.raWheel);
         context = getApplicationContext();
+        testRestaurants = new ArrayList<>();
+        testChosen = new ArrayList<>();
+        client = new YelpClient();
         clockwise = AnimationUtils.loadAnimation(context,R.anim.clockwise);
         clockwise1 = AnimationUtils.loadAnimation(context,R.anim.clockwise1);
         clockwise2 = AnimationUtils.loadAnimation(context,R.anim.clockwise2);
@@ -47,18 +59,25 @@ public class RandomizeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); //removes title
 
+        testRestaurants = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        Log.d("RandomActivity", "Recieved arraylist of size: " + testRestaurants.size());
+
         for(int i=0; i< chosen.length; i++){ //assigns restaurant to fixed array
-            chosen[i] = restaurants[random.nextInt(restaurants.length)];
+            //chosen[i] = restaurants[random.nextInt(testRestaurants.size())];
+            testChosen.add(testRestaurants.get(random.nextInt(testRestaurants.size())));
+            chosen[i] = testChosen.get(i).getName();
         }
 
-        if(restaurants.length >= chosen.length){
+        if(testRestaurants.size() >= chosen.length){
             //if the length of the array of restaurants
             // is more than the length of the chosen restaurants,
             // recheck for repeated restaurants
             for(int i=0; i<chosen.length; i++){ //replace pre-existing restaurants
                 for (int k = i + 1; k < chosen.length; k++) {
                     while (chosen[i] == chosen[k]){
-                        chosen[k] = restaurants[random.nextInt(restaurants.length)];
+                        testChosen.remove(k);
+                        testChosen.add(k, testRestaurants.get(random.nextInt(testRestaurants.size())));
+                        chosen[k] = testChosen.get(k).getName();
                     }
                 }
                 Log.d("RandomizeActivity", "Restaurant chosen: " + chosen[i]);
@@ -69,7 +88,9 @@ public class RandomizeActivity extends AppCompatActivity {
             for(int i=0; i<chosen.length; i++){ //replace pre-existing restaurants
                 for (int k = i + 1; k < chosen.length; k++) {
                     if (chosen[i] == chosen[k]){
-                        chosen[k] = restaurants[random.nextInt(restaurants.length)];
+                        testChosen.remove(k);
+                        testChosen.add(k, testRestaurants.get(random.nextInt(testRestaurants.size())));
+                        chosen[k] = testChosen.get(k).getName();
                     }
                 }
                 Log.d("RandomizeActivity", "Restaurant chosen: " + chosen[i]);
