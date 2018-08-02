cc2071045a624ea73b1ee23c13b9c875c1f6da81
Annamary Cartagena <anitac@fb.com>
2018-08-02 13:05:24 -0700

    Updating Dao

--- app/src/main/java/com/example/suhirtha/randomadventure/models/RestaurantDao.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/models/RestaurantDao.java
@@ -1,8 +1,18 @@
 package com.example.suhirtha.randomadventure.models;
 
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
 /**
  * Created by anitac on 8/2/18.
  */
 
 public interface RestaurantDao {
+    @Query("SELECT * FROM restautant")
+    List<DatabaseRestaurant> getAllRestaurants();
+
+    @Insert
+    void insertAll(DatabaseRestaurant... restaurants);
 }
