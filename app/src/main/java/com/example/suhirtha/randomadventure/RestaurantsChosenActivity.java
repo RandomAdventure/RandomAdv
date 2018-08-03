6ff80bb5e4c1f002e8bd9fc514cb3cf2f4dc5eab
Annamary Cartagena <anitac@fb.com>
2018-08-03 11:31:16 -0700

    Databases Almost done. Working and Showing

--- app/src/main/java/com/example/suhirtha/randomadventure/RestaurantsChosenActivity.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/RestaurantsChosenActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.example.suhirtha.randomadventure.models.DatabaseRestaurant;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantsChosenActivity extends AppCompatActivity {
@@ -19,28 +20,24 @@ public class RestaurantsChosenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_chosen);
+
         recyclerView = (RecyclerView) findViewById(R.id.recyclerRestaurant);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //do this in the background, wrap in background
+        //need this to insert data
+        DatabaseHelper db = Room.databaseBuilder(getApplicationContext(), DatabaseHelper.class, "saved_restaurants")
+                .allowMainThreadQueries() //TODO change this
+                .build();
+
+        restaurants = new ArrayList<DatabaseRestaurant>();
+        restaurants = db.restaurantDao().getAllRestaurants();
         adapter = new RestaurantAdapter(restaurants);
         recyclerView.setAdapter(adapter);
 
-        DatabaseHelper db = Room.databaseBuilder(getApplicationContext(), DatabaseHelper.class, "saved_restaurants")
-                .build();
 
-
-        restaurants = db.restaurantDao().getAllRestaurants();
-
-        //restaurants = new ArrayList<DatabaseRestaurant>();
-        // TO SHOW ALL DATABASES
-        showRestaurants();
-    }
-
-    private void showRestaurants() {
-
-        for (int i = 0; i < 5; i++) {
-            //DatabaseRestaurant restaurant = new DatabaseRestaurant("8dUaybEPHsZMgr1iKgqgMQ", "Sotto Mare Oysteria #" + i);
-            //restaurants.add(restaurant);
-        }
+        //USED TO INSERT INFO TO DATABASE
+        //db.restaurantDao().insertAll(new DatabaseRestaurant("8dUaybEPHsZMgr1iKgqgMQ", "TestRestaurantName3"));
     }
 
 }
