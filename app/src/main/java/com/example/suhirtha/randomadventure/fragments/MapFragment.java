93d9c9fa1a68a52f1956b124d4b83aab93e9b93a
Tatum Ogata <togata@fb.com>
2018-08-09 14:34:21 -0700

    updated map fragment

--- app/src/main/java/com/example/suhirtha/randomadventure/fragments/MapFragment.java
+++ app/src/main/java/com/example/suhirtha/randomadventure/fragments/MapFragment.java
@@ -65,52 +65,79 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap map) {
         googleMap = map;
         //TODO -- make i ok for it to only have one object in array and not throw error
-        double lowestLat;
-        double highestLat;
+        double lowestLat = 0;
+        double highestLat = 0;
         double lowestLong = 0;
         double highestLong = 0;
-        if (previousLocations.get(0).latitude<previousLocations.get(1).latitude){
-            lowestLat = previousLocations.get(0).latitude;
-            highestLat = previousLocations.get(1).latitude;
-        }
-        else{
-            lowestLat = previousLocations.get(1).latitude;
-            highestLat = previousLocations.get(0).latitude;
-        }
-        if (previousLocations.get(0).longitude<previousLocations.get(1).longitude){
-            lowestLong = previousLocations.get(0).longitude;
-            highestLong = previousLocations.get(1).longitude;
-        }
-        else{
-            lowestLong = previousLocations.get(1).longitude;
-            highestLong = previousLocations.get(0).longitude;
-        }
-        for (int i = 0; i < previousLocations.size(); i ++){
-            Marker marker = googleMap.addMarker(new MarkerOptions()
-                    .position(previousLocations.get(i))
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
-            dropPinEffect(marker);
-            if (previousLocations.get(i).latitude<lowestLat){
-                lowestLat = previousLocations.get(i).latitude;
+        if (previousLocations.size()>=2) {
+            if (previousLocations.get(0).latitude < previousLocations.get(1).latitude) {
+                lowestLat = previousLocations.get(0).latitude;
+                highestLat = previousLocations.get(1).latitude;
+            } else {
+                lowestLat = previousLocations.get(1).latitude;
+                highestLat = previousLocations.get(0).latitude;
             }
-            if (previousLocations.get(i).latitude>highestLat){
-                highestLat = previousLocations.get(i).latitude;
+            if (previousLocations.get(0).longitude < previousLocations.get(1).longitude) {
+                lowestLong = previousLocations.get(0).longitude;
+                highestLong = previousLocations.get(1).longitude;
+            } else {
+                lowestLong = previousLocations.get(1).longitude;
+                highestLong = previousLocations.get(0).longitude;
             }
-            if (previousLocations.get(i).longitude<lowestLong){
-                lowestLong = previousLocations.get(i).longitude;
-            }
-            if (previousLocations.get(i).longitude>highestLong){
-                highestLong = previousLocations.get(i).longitude;
-            }
+            for (int i = 0; i < previousLocations.size(); i++) {
+                Marker marker = googleMap.addMarker(new MarkerOptions()
+                        .position(previousLocations.get(i))
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+                dropPinEffect(marker);
+                if (previousLocations.get(i).latitude < lowestLat) {
+                    lowestLat = previousLocations.get(i).latitude;
+                }
+                if (previousLocations.get(i).latitude > highestLat) {
+                    highestLat = previousLocations.get(i).latitude;
+                }
+                if (previousLocations.get(i).longitude < lowestLong) {
+                    lowestLong = previousLocations.get(i).longitude;
+                }
+                if (previousLocations.get(i).longitude > highestLong) {
+                    highestLong = previousLocations.get(i).longitude;
+                }
 
+            }
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(new LatLng(lowestLat, lowestLong));
+            builder.include(new LatLng(highestLat, highestLong));
+            LatLngBounds bounds = builder.build();
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 80);
+            googleMap.animateCamera(cameraUpdate);
         }
 
-        LatLngBounds.Builder builder = new LatLngBounds.Builder();
-        builder.include(new LatLng(lowestLat, lowestLong));
-        builder.include(new LatLng(highestLat, highestLong));
-        LatLngBounds bounds = builder.build();
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 60);
-        googleMap.animateCamera(cameraUpdate);
+        else{
+            for (int i = 0; i < previousLocations.size(); i++) {
+                Marker marker = googleMap.addMarker(new MarkerOptions()
+                        .position(previousLocations.get(i))
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+                dropPinEffect(marker);
+                if (previousLocations.get(i).latitude < lowestLat) {
+                    lowestLat = previousLocations.get(i).latitude;
+                }
+                if (previousLocations.get(i).latitude > highestLat) {
+                    highestLat = previousLocations.get(i).latitude;
+                }
+                if (previousLocations.get(i).longitude < lowestLong) {
+                    lowestLong = previousLocations.get(i).longitude;
+                }
+                if (previousLocations.get(i).longitude > highestLong) {
+                    highestLong = previousLocations.get(i).longitude;
+                }
+
+            }
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            builder.include(new LatLng(lowestLat, lowestLong));
+            builder.include(new LatLng(highestLat, highestLong));
+            LatLngBounds bounds = builder.build();
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 80);
+            googleMap.animateCamera(cameraUpdate);
+        }
 
     }
 
